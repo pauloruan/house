f60bff97238814ff837fc5ce5c95993e57098aa5
Paulo Ruan <85769241+pauloruan@users.noreply.github.com>
2026-06-30 22:43:23 -0300

    feat: tornar sslmode do banco configuravel via DB_SSLMODE

--- api/cmd/app/main.go
+++ api/cmd/app/main.go
@@ -35,8 +35,12 @@ func main() {
 
 	jwtSecret := os.Getenv("JWT_SECRET")
 
-	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"))
+	sslmode := os.Getenv("DB_SSLMODE")
+	if sslmode == "" {
+		sslmode = "disable"
+	}
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
+		os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_USER"), os.Getenv("DB_PASSWORD"), os.Getenv("DB_NAME"), sslmode)
 
 	db, err := sql.Open("postgres", connStr)
 	if err != nil {
